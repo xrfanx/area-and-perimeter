2d31ea2105250ededed37fe224ccdb6a650d2b84
Xrfan <erfanroozbahani6@gmail.com>
2025-09-11 02:12:07 +0330

    Delete area and perimeter.py

--- area and perimeter.py
+++
@@ -1,7 +0,0 @@
-r = int(input('Enter the radius of the circle'))
-
-s = 3.14 * (r ** 2)
-l = 2 * 3.14 * r  
-
-print ('Area of a circle = ' , s , sep='')
-print ('Perimeter of a circle = ' , l , sep='')
